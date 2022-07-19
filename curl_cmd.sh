8def69032dbd80e96f2b425685c6223234c4b737
Aodan Xin <aodan.xin@faire.com>
2022-07-19 13:46:53 -0400

    Add client payload

--- curl_cmd.sh
+++ curl_cmd.sh
@@ -6,4 +6,4 @@ curl \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: token $GITHUB_TOKEN" \
   $URL \
-  -d '{"event_type":"remote_trigger"}'
+  -d '{"event_type":"remote_trigger", "client_payload": {"jira_ticket_id":"FD-1234"}}'
