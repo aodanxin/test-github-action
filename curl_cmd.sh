e3fbf5582265df67e7f80e18fd09a0f0eb64ad8b
Aodan Xin <aodan.xin@faire.com>
2022-07-28 10:20:59 -0400

    Update the sed command to only match the lines that end with the scenario name

--- curl_cmd.sh
+++ curl_cmd.sh
@@ -7,4 +7,4 @@ curl \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: token $GITHUB_TOKEN" \
   $URL \
-  -d '{"event_type":"ignore_e2e_pr", "client_payload": {"repo":"web-brand", "jira_ticket_id":"FD-1234", "file_path":"cypress/integration/testFiles/Analytics.feature", "scenario_name": "Filter data by a preset time interval", "reviewer":"bryan-holder-faire"}}'
+  -d '{"event_type":"ignore_e2e_pr", "client_payload": {"repo":"web-retailer", "jira_ticket_id":"MKG-1234", "file_path":"cypress/integration/testFiles/Cart/PreorderCart.feature", "scenario_name":"Full Cart","reviewer":"aodanxin"}}'
